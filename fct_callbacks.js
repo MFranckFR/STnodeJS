e06c2b41821db4567a0150b46c831ea54d25c3fa
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-01-14 12:11:15 +0100

    callback ex1:appartient v2

--- fct_callbacks.js
+++ fct_callbacks.js
@@ -1,9 +1,19 @@
 // Ecrire une fonction qui permet de vérfier si element appartient à tab.
 const tab = [1,3,6,8,9];
-const elt = 5;
-fctInclude = ((list,elt)=>list.some((e=>e==elt)))
+const elt = 9;
 
+fctInclude1 = ((list,elt)=>list.some((e=>e==elt)))
+
+function fctInclude2(list, elt){
+   let i = 0, m = false;
+   while(i < list.length){
+        if (list[i] == elt) {
+            m = true;
+        };
+        i++;
+   }
+   return m;
+}
 console.log(`${elt} appartient à ${tab} ? ${tab.includes(elt)?'oui':'non'}`);
-console.log(`${elt} appartient à ${tab} ? ${fctInclude(tab, elt)?'oui':'non'}`);
-
-
+console.log(`${elt} appartient à ${tab} ? ${fctInclude1(tab, elt)?'oui':'non'}`);
+console.log(`${elt} appartient à ${tab} ? ${fctInclude2(tab, elt)?'oui':'non'}`);
